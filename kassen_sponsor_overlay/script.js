5129a54b337ec8c562b287b77d70e8943a95c37f
moriji0320-dev <moriji0320@gmail.com>
2026-07-25 11:06:46 +0900

    Add sponsor display functionality

--- kassen_sponsor_overlay/script.js
+++ kassen_sponsor_overlay/script.js
@@ -1,1 +1,17 @@
+const sponsors = [
+  "🏆 合戦スポンサー募集中！",
+  "スポンサー①",
+  "スポンサー②",
+  "スポンサー③"
+];
 
+let i = 0;
+const el = document.getElementById("スポンサー表示");
+
+function showSponsor() {
+  el.textContent = sponsors[i];
+  i = (i + 1) % sponsors.length;
+}
+
+showSponsor();
+setInterval(showSponsor, 3000);
